2fd9300b5576123438ce9c5a6dcc6b4a25febb24
kmarabet <kamilmarabet@gmail.com>
2020-10-19 23:22:01 +0300

    Lesson_2_7 add Settings using ini4j

--- src/com/company/PersonalFinance.java
+++ src/com/company/PersonalFinance.java
@@ -1,9 +1,8 @@
 package com.company;
 
+import com.company.settings.Settings;
 import com.company.settings.Text;
-
 import java.awt.*;
-import java.io.File;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,20 +11,22 @@ public class PersonalFinance {
 
     public static void main(String[] args) {
         init();
-	// write your code here
+        // write your code here
         System.out.println(Text.get("PROGRAM_NAME"));
         System.out.println(Text.getMonths());
     }
 
     private static void init() {
-        Text.init();
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         try {
-            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Consolas.ttf")));
+            Settings.init();
+//            Settings.save();
+            Text.init();
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+//            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Consolas.ttf")));
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, Settings.FONT_CONSOLAS));
         } catch (FontFormatException | IOException e) {
             Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, e);
 //            e.printStackTrace();
-
         }
     }
 }
---
+++ src/com/company/settings/Settings.java
@@ -0,0 +1,72 @@
+package com.company.settings;
+
+import com.company.PersonalFinance;
+import org.ini4j.Ini;
+import org.ini4j.IniPreferences;
+import org.ini4j.Wini;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+
+public class Settings {
+
+    public static final File FONT_CONSOLAS = new File("fonts/Consolas.ttf");
+    public static final File SAVE_DIR = new File("saves/");
+    public static final String SAVE_FILE_EXT = "myrus";
+
+    public static final String FORMAT_AMOUNT = "%.2f";
+    public static final String FORMAT_RATE = "%.4f";
+    public static final String FORMAT_DATE = "dd.MM.yyyy";
+    public static final String FORMAT_DATE_MONTH = "MMMM.yyyy";
+    public static final String FORMAT_DATE_YEAR = "yyyy";
+
+    public static final int COUNT_OVERVIEW_ROWS = 10;
+
+    public static final String[] CURRENCIES_CODES = new String[]{"RUB", "USD", "EUR", "BYN", "UAH"};
+
+    public static final File FILE_SETTINGS = new File("saves/settings.ini");
+    public static File FILE_SAVE = new File("saves/default.myrus");
+
+    public static void init() {
+        try {
+            Ini ini = new Ini(FILE_SETTINGS);
+            Preferences prefs = new IniPreferences(ini);
+            String file = prefs.node("Settings").get("SAVE_FILE", null);
+            if (file != null) FILE_SAVE = new File(file);
+//            System.out.println(FILE_SAVE.getAbsolutePath());
+            setLocal();
+        } catch (IOException e) {
+            save();
+//            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, e);
+//            e.printStackTrace();
+        }
+    }
+
+    public static File getFileSave() {
+        return FILE_SAVE;
+    }
+
+    public static void setFileSave(File fileSave) {
+        FILE_SAVE = fileSave;
+        save();
+    }
+
+    public static void save() {
+        try {
+            Wini ini = new Wini(FILE_SETTINGS);
+            ini.put("Settings", "FILE_SAVE", FILE_SAVE.getAbsolutePath().replace("\\", "\\\\"));
+            ini.store();
+        } catch (IOException e) {
+            Logger.getLogger(PersonalFinance.class.getName()).log(Level.SEVERE, null, e);
+//            e.printStackTrace();
+        }
+    }
+
+    private static void setLocal() {
+        Locale.setDefault(new Locale("ru"));
+    }
+}
