54f72cfcff60d3e12ba915b1fd87686d1d5c875f
kmarabet <kamilmarabet@gmail.com>
2020-10-20 16:03:39 +0300

    Lesson_2_10 add Code Handler class for event handling.

---
+++ src/com/company/settings/HandlerCode.java
@@ -0,0 +1,38 @@
+package com.company.settings;
+
+public class HandlerCode {
+
+    public static final String MENU_FILE_NEW = "MENU_FILE_NEW";
+    public static final String MENU_FILE_OPEN = "MENU_FILE_OPEN";
+    public static final String MENU_FILE_SAVE = "MENU_FILE_SAVE";
+    public static final String MENU_FILE_UPDATE_CURRENCIEW = "MENU_FILE_UPDATE_CURRENCIEW";
+    public static final String MENU_FILE_EXIT = "MENU_FILE_EXIT";
+    public static final String MENU_FILE_ADD = "MENU_FILE_ADD";
+    public static final String MENU_FILE_EDIT = "MENU_FILE_EDIT";
+    public static final String MENU_FILE_DELETE = "MENU_FILE_DELETE";
+    public static final String MENU_FILE_OVERVIEW = "MENU_FILE_OVERVIEW";
+    public static final String MENU_FILE_ACCOUNTS = "MENU_FILE_ACCOUNTS";
+    public static final String MENU_FILE_TRANSACTIONS = "MENU_FILE_TRANSACTIONS";
+    public static final String MENU_FILE_TRANSFERS = "MENU_FILE_TRANSFERS";
+    public static final String MENU_FILE_CURRENCIES = "MENU_FILE_CURRENCIES";
+    public static final String MENU_FILE_STATISTICS = "MENU_FILE_STATISTICS";
+    public static final String MENU_FILE_ABOUT = "MENU_FILE_ABOUT";
+
+    public static final String TOOLBAR_OVERVIEW = "TOOLBAR_OVERVIEW";
+    public static final String TOOLBAR_ACCOUNTS = "TOOLBAR_ACCOUNTS";
+    public static final String TOOLBAR_ARTICLES = "TOOLBAR_ARTICLES";
+    public static final String TOOLBAR_TRANSACTIONS = "TOOLBAR_TRANSACTIONS";
+    public static final String TOOLBAR_TRANSFERS = "TOOLBAR_TRANSFERS";
+    public static final String TOOLBAR_CURRENCIES = "TOOLBAR_CURRENCIES";
+    public static final String TOOLBAR_STATISTICS = "TOOLBAR_STATISTICS";
+
+    public static final String ADD = "ADD";
+    public static final String EDIT = "EDIT";
+    public static final String DELETE = "DELETE";
+    public static final String CANCEL = "CANCEL";
+    public static final String LEFT = "LEFT";
+    public static final String RIGHT = "RIGHT";
+    public static final String STEP = "STEP";
+    public static final String TYPE = "TYPE";
+
+}
